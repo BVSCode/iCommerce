a2bc766daf64fcb6622ebcfdd766a45eb5749f95
BVSCode <bhaveshkumarchandravyash@gmail.com>
2023-10-09 13:15:03 +0530

    Implemented-- cathcAsync to handle Promise rejection and get rid of try catch block.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const productRouter = require('./routes/productRoute');
 
 const app = express();
@@ -17,26 +19,13 @@ app.use(express.json());
 // Availble-Routes || End-Points
 app.use('/api/v1/products', productRouter);
 
+// Handling undefined routes
 app.all('*', (req, res, next) => {
-    // Creating an Error and pass it to the global error handler
-    // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-    // err.status = 'fails';
-    // err.statusCode = 404;
-    // next(err);
-
     // Creating an Error here and passing to Global Error handler
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-    // console.log(err.stack);
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-});
+// Error and Exception Handling
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require('../models/productModel');
 const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopProducts = (req, res, next) => {
     req.query.limit = '5';
@@ -8,76 +9,62 @@ exports.aliasTopProducts = (req, res, next) => {
     next();
 }
 
-exports.getAllProducts = async (req, res) => {
-    try {
-        // Build the query
-        // 1A) Filtering
-        const queryObj = { ...req.query }; //creating a copy of query obj and exclude fields
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
+exports.getAllProducts = catchAsync(async (req, res, next) => {
+    // Build the query
+    // 1A) Filtering
+    const queryObj = { ...req.query }; //creating a copy of query obj and exclude fields
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-        // 1B) Advance Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    // 1B) Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-        let query = Product.find(JSON.parse(queryStr)); //save the mongoose query to chain sort limit etc methods after
+    let query = Product.find(JSON.parse(queryStr)); //save the mongoose query to chain sort limit etc methods after
 
-        // 2) Sorting 
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        }
-        else {
-            query = query.sort('-createdAt'); // sort by newest
-        }
-
-        // Fields Limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        }
-        else {
-            query = query.select('-__v');
-        }
-
-        // 4) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-        console.log(skip, limit);
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Product.countDocuments();
-            if (skip >= numTours) throw new Error('This page does not Exits');
-        }
-
-        // Now Execute the query
-        const products = await query;
-
-        res.status(200).json({
-            status: "success",
-            results: products.length,
-            data: products
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: "fails",
-            message: error
-        })
+    // 2) Sorting 
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
     }
-}
+    else {
+        query = query.sort('-createdAt'); // sort by newest
+    }
 
-exports.getProduct = async (req, res, next) => {
+    // Fields Limiting
+    if (req.query.fields) {
+        const fields = req.query.fields.split(',').join(' ');
+        query = query.select(fields);
+    }
+    else {
+        query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    console.log(skip, limit);
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+        const numTours = await Product.countDocuments();
+        if (skip >= numTours) return next(new AppError('Page not found!', 404));
+    }
+
+    // Now Execute the query
+    const products = query;
+
+    res.status(200).json({
+        status: "success",
+        results: products.length,
+        data: products
+    });
+});
+
+exports.getProduct = catchAsync(async (req, res, next) => {
     const product = await Product.findById(req.params.id);
-
-    // if (!product) {
-    //     const err = new Error(`Product not found!`);
-    //     err.status = 'fails';
-    //     err.statusCode = 404;
-    //     return next(err);
-    // }
 
     if (!product) {
         return next(new AppError('Product not found with that ID!', 404));
@@ -87,25 +74,18 @@ exports.getProduct = async (req, res, next) => {
         status: "success",
         data: product
     });
-}
+});
 
-exports.createProduct = async (req, res) => {
-    try {
-        const newProduct = await Product.create(req.body)
+exports.createProduct = catchAsync(async (req, res, next) => {
+    const newProduct = await Product.create(req.body);
 
-        res.status(201).json({
-            status: "success",
-            data: newProduct
-        });
-    } catch (error) {
-        res.status(400).json({
-            status: "fails",
-            message: error
-        })
-    }
-}
+    res.status(201).json({
+        status: "success",
+        data: newProduct
+    });
+});
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsync(async (req, res, next) => {
     const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
     if (!updatedProduct) {
@@ -116,9 +96,9 @@ exports.updateProduct = async (req, res, next) => {
         status: "success",
         data: updatedProduct
     });
-}
+});
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsync(async (req, res, next) => {
     const product = await Product.findByIdAndDelete(req.params.id);
 
     if (!product) {
@@ -129,9 +109,9 @@ exports.deleteProduct = async (req, res, next) => {
         status: "success",
         data: null
     });
-}
+});
 
-// Aggregation Pipeline--
+// Aggregation Pipeline-- Product Stastics--
 exports.getProductStats = async (req, res) => {
     try {
         const stats = await Product.aggregate([
