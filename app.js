e90710f53cc328737a57be1e8a99e17830804c32
BVSCode <bhaveshkumarchandravyash@gmail.com>
2023-10-10 18:06:13 +0530

    Implemented-- Security best practices to prevent xss, cors etc..

--- app.js
+++ app.js
@@ -1,24 +1,71 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
+// ERROR HANDLING FILES-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
+// ROUTERS FILES-
 const productRouter = require('./routes/productRoute');
 const userRouter = require('./routes/userRoute');
 
 const app = express();
 
-// DEV-MIDDLEWARES
+// Enable Proxy for app
+app.enable('trust proxy');
+
+// Enable Cors
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.iCommerce.com, front-end iCommerce.com
+// app.use(cors({
+//   origin: 'https://www.iCommerce.com'
+// }))
+
+// Enable Cors pre-flight for All Request-
+app.options('*', cors());
+// app.options('/api/v1/products/:id', cors());
+
+// Set Security HTTP headers
+app.use(helmet());
+
+// Dev - Loggin
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
-// PROD-MIDDLEWARES
-app.use(express.json());
-app.use(cookieParser());
+// Limit Requests from same API-
+const limiter = rateLimit({
+  max: 500, // max request
+  windowMs: 60 * 60 * 1000, // max request in this time frame
+  message: 'Too many requests from this IP, please try again in an hour!' // generic error message
+});
+app.use('/api', limiter); // Set Limiter to api /api - routes
 
+// Body Parser-
+app.use(express.json({ limit: '10kb' })); // limiting 10kb data in req.body
+
+// Parsing the req.body based on urlencoded-
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+// Cookie Parser-
+app.use(cookieParser()); 
+
+// Data Sanitization Against NoSQL query injection-
+app.use(mongoSanitize());
+
+// Data Sanitization Against XSS - cross-side-scripting attacks
+app.use(xss()); // remove all the malious html with javascript code at req.body
+
+// Compression the app to better perfomance and optimazation-
+app.use(compression());
 
 // Availble-Routes || End-Points
 app.use('/api/v1/products', productRouter);
